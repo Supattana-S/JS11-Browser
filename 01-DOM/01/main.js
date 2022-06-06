d91a3274162039bfce3a1884ad05105c0a2d9e98
Supattana-S <supattana.sue@gmail.com>
2022-06-06 09:21:51 +0700

    Done lab01.01

---
+++ 01-DOM/01/main.js
@@ -0,0 +1,11 @@
+const div = document.querySelector("div");
+const ul = document.querySelector("ul");
+const li1 = ul.querySelector("li");
+const li2 = li1.nextElementSibling;
+
+div.style.backgroundColor = "red";
+ul.style.backgroundColor = "green";
+li1.style.backgroundColor = "blue";
+li2.style.backgroundColor = "coral ";
+
+console.log(div);

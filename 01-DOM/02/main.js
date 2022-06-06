72748f27ab65b0f01d414daa40988a13b5ff2c2d
Supattana-S <supattana.sue@gmail.com>
2022-06-06 09:25:49 +0700

    Done lab01.02

---
+++ 01-DOM/02/main.js
@@ -0,0 +1,6 @@
+// const h2 = document.querySelector("h2");
+// const h2 = document.querySelectorAll("h2"); //h2[0]
+// const h2 = document.getElementsByClassName("title-1"); //h[0]
+const h2 = document.getElementsByTagName("h2"); //h[0]
+
+console.log(h2[0]);

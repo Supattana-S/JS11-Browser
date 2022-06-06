be05da3777bf035a9f0bbc70d520fd46fa8e17be
Supattana-S <supattana.sue@gmail.com>
2022-06-06 09:49:50 +0700

    Done lab01.03

---
+++ 01-DOM/03/main.js
@@ -0,0 +1,6 @@
+// const li = document.querySelector("#\\33 ");
+// const li = document.querySelectorAll("li")[2];
+// const li = document.getElementById("3");
+const li = document.getElementsByTagName("li")[2];
+
+console.log(li);

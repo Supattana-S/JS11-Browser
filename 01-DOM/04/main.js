60a7142b27057fcd89596920b0bec2826577d101
Supattana-S <supattana.sue@gmail.com>
2022-06-06 09:54:00 +0700

    Done lab01.04

---
+++ 01-DOM/04/main.js
@@ -0,0 +1,7 @@
+const li = document.querySelector(".active");
+// const li = document.querySelectorAll("li")[1];
+// const li = document.getElementsByClassName("active")[0];
+// const li = document.getElementById("2");
+// const li = document.getElementsByTagName("li")[1];
+
+console.log(li);
